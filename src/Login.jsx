a47f5ac40390ab51fffda2c9c89d21796589093b
usmanazeem00 <usmanazeem00@gmail.com>
2023-11-09 15:13:29 +0500

    new changes login page

---
+++ src/Login.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="card">
+            <div className="card-body">
+              <h2 className="card-title text-center">Login</h2>
+              <form>
+                <div className="form-group">
+                  <label htmlFor="username">Username</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="username"
+                    placeholder="Enter your username"
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="password">Password</label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="password"
+                    placeholder="Enter your password"
+                  />
+                </div>
+                <button type="submit" className="btn btn-primary btn-block">
+                  Login
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- src/New.tsx
+++ src/New.tsx
@@ -2,12 +2,14 @@ import React from "react"
 import AppFunc from "./AppFunc.tsx"
 import AppClass from "./AppClass.tsx"
 import Display from "./Display.jsx"
+import Login from "./Login.jsx"
 function New()
 {
 return(
 //   {  <AppFunc emotion={"sad"} Age={33} Gender={"MALE"}/>}
 // {<AppClass emotion={"sad"} Age={33}/>}
-<Display name="Usman" age="21"/>
-)
+//<Display name="Usman" age="21"/>Log
+<Login/>
+);
 }
 export default New
