0e832914e89259ac33953555112778eef69b94e0
usmanazeem00 <usmanazeem00@gmail.com>
2023-11-09 15:36:04 +0500

    new channge

--- src/Login.jsx
+++ src/Login.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Login = () => {
   return (
-    <div className="container mt-5">
+    <>
+    <form action=''>
+    <div className="container mt-5" style={{}}>
       <div className="row justify-content-center">
         <div className="col-md-6">
           <div className="card">
@@ -10,22 +12,30 @@ const Login = () => {
               <h2 className="card-title text-center">Login</h2>
               <form>
                 <div className="form-group">
-                  <label htmlFor="username">Username</label>
+                  <label htmlFor="username" style={{fontWeight:'600'}}>Username</label>
                   <input
                     type="text"
                     className="form-control"
                     id="username"
                     placeholder="Enter your username"
+                    autoComplete='off'
+                    required
                   />
                 </div>
                 <div className="form-group">
-                  <label htmlFor="password">Password</label>
+                  <label htmlFor="password" style={{fontWeight:'600'}}>Password</label>
                   <input
                     type="password"
                     className="form-control"
                     id="password"
-                    placeholder="Enter your password"
+                    minLength={8}
+                    required
+                    placeholder="Enter your password (Min 8 Length)"
                   />
+                </div>
+                <div className="row justify-content-center">
+                <p style={{fontWeight:'600'}}>Don't have an account?</p>&nbsp;
+                <a href="https://google.com" style={{color:'blue'}}>SignUp</a>
                 </div>
                 <button type="submit" className="btn btn-primary btn-block">
                   Login
@@ -36,6 +46,8 @@ const Login = () => {
         </div>
       </div>
     </div>
+    </form>
+    </>
   );
 };
 
