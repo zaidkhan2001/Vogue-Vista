9944005ea8254f4e9be2b401aca60053bf34a88c
usmanazeem00 <usmanazeem00@gmail.com>
2023-11-09 13:15:12 +0500

    new chzzng

--- src/AppFunc.tsx
+++ src/AppFunc.tsx
@@ -8,7 +8,7 @@ interface AppFuncProps {
 
 function AppFunc(obj:AppFuncProps) {
   const [name, setName] = useState("");
-  const [age, setAge] = useState(16);
+  const [age, setAge] = useState(1000);
   const [isMale, setIsMale] = useState(true);
  
   return (
